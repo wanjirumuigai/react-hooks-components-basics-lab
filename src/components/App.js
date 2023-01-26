785ad28f204866b39632ca9860328b26313f8f47
Pauline Muigai <wanjirumuigai@gmail.com>
2023-01-26 13:43:14 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,23 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
-{/* write an <About> component here */}
+{<About/>}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {<Home/>}
+      {<About/>}
     </div>
   );
 }
